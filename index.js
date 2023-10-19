742521e9079de10c18fe4cb7889f3ee0f0cf154f
IshaqQuresheeAkib <akibqureshee@gmail.com>
2023-10-19 11:04:41 +0600

    completed api for deleting items from carts

--- index.js
+++ index.js
@@ -78,12 +78,7 @@ async function run() {
             console.log(id);
             const query = {_id: id}
             const result = await cartsCollection.deleteOne(query)
-            res.send(result)
-            if (result.deletedCount === 1) {
-                console.log("Successfully deleted one document.");
-              } else {
-                console.log("No documents matched the query. Deleted 0 documents.");
-              }
+            res.send(result)            
         })
 
         console.log("Pinged your deployment.You successfully connected to MongoDb!");
